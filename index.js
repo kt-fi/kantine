5b65b26c5a17f74931a66b90e9a427aac4d50358
kt-fi <49416309+kt-fi@users.noreply.github.com>
2019-10-18 16:32:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
+var lives = 5;
+var rings = 0;
 
-var result = document.getElementById("resultBar");
-result= 0;
+var btnOne = document.getElementById("btnOne").addEventListener("click", function(){
+  document.getElementById('placeholder').src = "placeholder2.jpg";
 
+  rings = rings + 10;
+  document.getElementById("rings").innerHTML = rings;
 
-
-document.getElementById("one").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 1;
-    result = result + 1;
 });
 
-document.getElementById("two").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 2;
-    result = result + 2;
-});
+var btnTwo = document.getElementById("btnTwo").addEventListener("click", function(){
+  document.getElementById('placeholder').src = "placeholder2.jpg";
 
-document.getElementById("three").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 3;
-    result = result + 3;
-});
+  lives = lives - 1;
+  document.getElementById("lives").innerHTML = lives;
 
-document.getElementById("four").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 4;
-    result = result + 4;
-});
-
-document.getElementById("five").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 5;
-    result = result + 5;
-});
-
-document.getElementById("six").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 6;
-    result = result + 6;
-});
-
-document.getElementById("seven").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 7;
-    result = result + 7;
-});
-
-document.getElementById("eight").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 8;
-    result = result + 8;
-});
-
-document.getElementById("nine").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = result + 9;
-    result = result + 9;
-});
-
-document.getElementById("clear").addEventListener("click", function(){
-    document.getElementById("resultBar").innerHTML = 0;
-    result = 0;
 });
