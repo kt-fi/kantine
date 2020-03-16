af1f46f9d324428fef628674300585014f98e5d8
kt-five <katie5five.5@gmail.com>
2020-03-16 03:10:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-$(".enterBtn").click(function(){
-  $(".intro").slideToggle(2000);
-  $(".title").slideDown(2000);
-  $(".navbar").slideToggle(2000);
-  $("main").slideDown(2000);
-});
+$(window).scroll(function(){
 
-$(".navbar").hide();
-$(".title").hide();
-$("main").hide();
+    if(scrollY > 40){
+        $(".mynavbar__logo").css('width', '8rem');
+        $(".mynavbar__logo").css('margin-bottom', '1rem');
+        $(".mynavbar__logo").css('margin-top', '1rem');
+    
+        $(".mynavbar__container").css('background', 'rgba(0, 0, 0, .95)')
+        
+        $(".mynavbar__links--1").css('padding-bottom', '1rem');
+        $(".mynavbar__links--1").css('margin-bottom', '2rem');
+        $(".mynavbar__links--1").css('margin-top', '2rem');
+
+    }if(scrollY === 0){
+        $(".mynavbar__logo").css('width', '10rem');
+        $(".mynavbar__logo").css('margin-bottom', '3rem');
+        $(".mynavbar__logo").css('margin-top', '3rem');
+    
+        $(".mynavbar__container").css('background', 'rgba(0, 0, 0, .8)')
+         
+        $(".mynavbar__links--1").css('padding-bottom', '2rem');
+        $(".mynavbar__links--1").css('margin-bottom', '3rem');
+        $(".mynavbar__links--1").css('margin-top', '3rem');
+
+    }
+        
+    
 
  
+})
+
