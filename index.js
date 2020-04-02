046fffb71e157b87a0521fd8749c5ac5e94b5bbc
kt-five <katie5five.5@gmail.com>
2020-04-02 16:58:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 
+$(window).on("orientationchange",function(){
+ 
+$(document).ready(function(){
+
+    var stickyNavTop = $(".navbarK").offset().top;
+
+    var stickyNav = function(){
+    var scrollTop = $(window).scrollTop()
+
+   if (scrollTop > stickyNavTop){
+        $(".navbarK").addClass("sticky");
+        $(".main").css("transform", "translateY(40rem)")
+    }else{
+        $(".navbarK").removeClass("sticky");
+        $(".main").css("transform", "translateY(0rem)")
+    }
+} 
+
+stickyNav();
+$(window).scroll(function(){
+    stickyNav();
+
+    
+})
+} )
+  });
 
 // STICKY NAVBAR
 
