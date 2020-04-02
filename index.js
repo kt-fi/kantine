f4ced3ff5d4eb57e4c2b7aba78c533b98746fa44
kt-five <katie5five.5@gmail.com>
2020-04-02 17:11:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 
 $(window).on("orientationchange",function(){
- 
-$(document).ready(function(){
 
-    var stickyNavTop = $(".navbarK").offset().top;
-
-    var stickyNav = function(){
-    var scrollTop = $(window).scrollTop()
-
-   if (scrollTop > stickyNavTop){
-        $(".navbarK").addClass("sticky");
-        $(".main").css("transform", "translateY(40rem)")
-    }else{
-        $(".navbarK").removeClass("sticky");
-        $(".main").css("transform", "translateY(0rem)")
-    }
-} 
-
-stickyNav();
-$(window).scroll(function(){
-    stickyNav();
-
-    
-})
-} )
+    alert("ROTATED")
+    //you can get the screen size
+//     var windowWidth = $(window).width(),
+//     var windowHeight = $(window).height();
+//     //some code
   });
 
 // STICKY NAVBAR
