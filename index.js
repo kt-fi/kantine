1f96ea23ffc093e8074ef30b08d1dddc2c6ad1cf
kt-five <katie5five.5@gmail.com>
2020-04-02 05:36:35 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -28,11 +28,3 @@ $(window).scroll(function(){
 jQuery(document).ready(function($){
     orientationChange();
 });
-
-function orientationChange() {
-    if(window.addEventListener) {
-        window.addEventListener("orientationchange", function() {
-            location.reload();
-        });
-    }
-}
