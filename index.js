2a0ecac131c69b8cc2c937dc974c4509e6419f4d
kt-five <katie5five.5@gmail.com>
2020-04-04 00:18:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,92 @@
+ 
+
+ 
+ //NAVBAR MOBILE
+var width = $(document).width();
+
+function smallScreen(){
+    $(".navbarKDrop").css("transform", "translateY(0rem)")
+
+    $(".navbarKDrop").hide();
+
+    $(".navbarK__links--link").hide();
+    $(".navbarK__links--brand").show();
+    $(".navbarK__links--hamburger").show();
+    $(".navbarK").removeClass("navBarK");
+    $(".navbarK").addClass("navLinkMobile");
+};
+
+function bigScreen(){
+    $(".navbarKDrop").hide();
+
+    $(".navbarK__links--link").show();
+    $(".navbarK").addClass("navBarK");
+    $(".navbarK").removeClass("navLinkMobile");
+    $(".navbarK__links--hamburger").hide();
+    $(".navbarK__links--brand").hide();
+}
+
+function dropDownNav(){
+
+}
+
+$(document).ready(function(){
+    width = $(document).width();
+    if( width < 750 ){
+     smallScreen();
+    }else{
+        bigScreen();
+    }
+})
+
+
+$(window).on("resize", function(){
+   width = $(document).width();
+   if( width < 750 ){
+        smallScreen();
+   }else{
+        bigScreen();
+   }
+});
+
+
+
+$(".navLinksToggle").click(function(){
+
+    
+        $(".navbarKDrop").slideToggle();
+
+    
+    
+})
+
+
+
 
 // STICKY NAVBAR
-alert("Navbar should work with no bugs finally")
-
 $(document).ready(function(){
 
     var stickyNavTop = $(".navbarK").offset().top;
-
- 
-    
+    var stickyNavTop = $(".navbarKDrop").offset().top;
 
     var stickyNav = function(){
     var scrollTop = $(window).scrollTop()
-     
+    var headerHeight = screen.height;
 
-    
-      var headerHeight = screen.height;
     $('.header').css('height', headerHeight)
 
    if (scrollTop > stickyNavTop){
         $(".navbarK").addClass("sticky");
-        $(".main").css("transform", "translateY(40rem)")
-       $('.header').css('height', headerHeight)
-
-       
+        $(".navbarKDrop").addClass("sticky");
+        $('.header').css('height', headerHeight)
+    $(".navbarK__links--linkDrop").css("transform", "translateY(40rem)")
        
     }else{
         $(".navbarK").removeClass("sticky");
-        $(".main").css("transform", "translateY(0rem)")
+        $(".navbarKDrop").removeClass("sticky");
         $('.header').css('height', headerHeight)
+        $(".navbarK").css("margin-top", "-40rem")
+        
     }
 } 
 
